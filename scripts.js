aee16e99a0783ad9b40837e430051da6f362d92a
Malmie <miechurska.m88@gmail.com>
2017-10-30 17:48:40 +0100

    new repository

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var a = 1;
+var b = 2;
+var value = (a * a) - (2 * a * b) + (b * b);
+
+console.log('wynik wynosi:' + value);
+
+if (value > 0) {
+  
+  console.log('wynik dodatni');
+  
+} else if (value < 0) {
+  
+  console.log('wynik ujemny');
+  
+} else {
+  
+  console.log('wynik równy 0');
+  
+}
+
+
